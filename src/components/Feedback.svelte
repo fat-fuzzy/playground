42c6aecad6408003b6ca8e7e969e7104830afd9e
patiboh <lapatiboh@gmail.com>
2021-03-20 10:54:56 +0100

    💄 Hide feedback by default

--- src/components/Feedback.svelte
+++ src/components/Feedback.svelte
@@ -1,14 +1,18 @@
 <script>
-  import {onMount} from 'svelte'
+  import * as constants from '../types/constants.js'
 
-  import {uiState} from '../stores.js'
+  import { onMount } from 'svelte'
+
+  import { uiState } from '../stores.js'
   // UI feedback
   export let stacktrace = ''
 
   let playgroundState
+  let hidden
 
   const uiStateUnsub = uiState.subscribe((value) => {
     playgroundState = value
+    hidden = playgroundState !== constants.uiState.ERROR ? 'hidden' : ''
   })
 
   onMount(() => {
@@ -18,10 +22,12 @@
   })
 </script>
 
-<div class={`feedback ${playgroundState}`} data-cy="feedback">
+<div class={`feedback ${hidden}`} data-cy="feedback">
   <pre class="stacktrace" data-cy="stacktrace">{stacktrace}</pre>
 </div>
 
-<style lang="scss">
-  @import '../styles/feedback.scss';
+<style>
+  .hidden {
+    display: none;
+  }
 </style>
